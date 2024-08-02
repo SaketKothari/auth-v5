8b7a50ec4b07fbb93c43059eec0bd6307eded798
Saket Kothari <saketkothari722@gmail.com>
2024-08-02 16:59:43 +0530

    🔗 Integrate Google & GitHub OAuth & add event handling for linking provider accounts to user database

--- auth.ts
+++ auth.ts
@@ -6,6 +6,14 @@ import authConfig from '@/auth.config';
 import { getUserById } from '@/data/user';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() },
+      });
+    },
+  },
   callbacks: {
     async session({ token, session }) {
       if (token.sub && session.user) {
