49e62d726daf791ffd1cda86fea8a3248c106a0f
Saket Kothari <saketkothari722@gmail.com>
2024-08-05 18:05:39 +0530

    🛠️ Add utility functions for db retrieval

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -2,6 +2,34 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { db } from '@/lib/db';
 import { getVerificationTokenByEmail } from '@/data/verification-token';
+import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
+
+export const generatePasswordResetToken = async (email: string) => {
+  const token = uuidv4();
+  // expiring the token in 1h
+  const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+  const existingToken = await getPasswordResetTokenByEmail(email);
+
+  // check if existing token sent to email
+  if (existingToken) {
+    await db.passwordResetToken.delete({
+      where: {
+        id: existingToken.id,
+      },
+    });
+  }
+
+  const passwordResetToken = await db.passwordResetToken.create({
+    data: {
+      email,
+      token,
+      expires,
+    },
+  });
+
+  return passwordResetToken;
+};
 
 export const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
